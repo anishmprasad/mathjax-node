8541409884f2ce17d8bac36dc8a6377cdc4a5fd2
Anish.M.Prasad <anish.m.prasad@gmail.com>
2019-12-10 17:27:58 +0530

    mathjax node

--- mathjax.js
+++ mathjax.js
@@ -6,55 +6,45 @@ mjAPI.config({
 	}
 });
 mjAPI.start();
-// var yourMath = 'E = mc^2';
-var yourMath = `<math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
-   <mi>E</mi>
-   <mo>=</mo>
-   <mi>m</mi>
-   <msup>
-     <mi>c</mi>
-     <mn>2</mn>
-   </msup>
- </math>`;
 
-function MathJax(math) {
+function mathJax(math) {
 	return mjAPI.typeset({
 		math: math,
 		format: 'MathML', // or "inline-TeX", "MathML"
 		svg: true // or svg:true, or html:true
 	});
-	try {
-		return mjAPI.typeset(
-			{
-				math: math,
-				format: 'MathML', // or "inline-TeX", "MathML"
-				svg: true // or svg:true, or html:true
-			},
-			mathRender
-		);
-	} catch (error) {
-		return {
-			success: false,
-			data: error
-		};
-	}
+	// try {
+	// 	return mjAPI.typeset(
+	// 		{
+	// 			math: math,
+	// 			format: 'MathML', // or "inline-TeX", "MathML"
+	// 			svg: true // or svg:true, or html:true
+	// 		},
+	// 		mathRender
+	// 	);
+	// } catch (error) {
+	// 	return {
+	// 		success: false,
+	// 		data: error
+	// 	};
+	// }
 }
 
-function mathRender(data) {
-	if (!data.errors) {
-		console.log(data.svg);
-		return { success: true, data: data.svg };
-	} else return { success: false, data: data.errors };
-	// will produce:
-	// <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
-	//   <mi>E</mi>
-	//   <mo>=</mo>
-	//   <mi>m</mi>
-	//   <msup>
-	//     <mi>c</mi>
-	//     <mn>2</mn>
-	//   </msup>
-	// </math>
-}
+// function mathRender(data) {
+// 	if (!data.errors) {
+// 		console.log(data.svg);
+// 		return { success: true, data: data.svg };
+// 	} else return { success: false, data: data.errors };
+// 	// will produce:
+// 	// <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+// 	//   <mi>E</mi>
+// 	//   <mo>=</mo>
+// 	//   <mi>m</mi>
+// 	//   <msup>
+// 	//     <mi>c</mi>
+// 	//     <mn>2</mn>
+// 	//   </msup>
+// 	// </math>
+// }
 
-module.exports = MathJax;
+module.exports = mathJax;
--- server.js
+++ server.js
@@ -3,42 +3,36 @@ var express = require('express'),
 	port = process.env.PORT || 3003,
 	bodyParser = require('body-parser');
 var cors = require('cors');
-var MathJax = require('./mathjax.js');
+var mathJax = require('./mathjax.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
-app.use('/data', data);
-// app.post("/", function(req, res) {
-//   res.send("hello world");
-// });
-// POST method route
 app.post('/math', math);
 
 app.use(function(req, res) {
 	res.status(404).send({ url: req.originalUrl + ' not found' });
 });
 
+function math(req, res) {
+	var math = req.query.math || '';
+	try {
+		mathJax(math)
+			.then(function(data) {
+				if (!data.errors) {
+					// console.log(data.svg);
+					res.status(200).json({ success: true, data: data.svg });
+				} else res.status(500).json({ success: false, data: data.errors });
+			})
+			.catch(function(error) {
+				res.status(500).json({ success: false, data: error });
+			});
+	} catch (error) {
+		res.status(500).json({ success: false, data: error });
+	}
+}
+
 app.listen(port);
 
 console.log('API server started on: ' + port);
-
-function data(req, res) {
-	return res.status(200).json({ success });
-}
-
-function math(req, res) {
-	renderedata(req.query.math, function(data) {
-		if (!data.errors) {
-			// console.log(data.svg);
-			res.status(500).json({ success: true, data: data.svg });
-		} else res.status(200).json({ success: false, data: data.errors });
-	});
-}
-
-function renderedata(math, callback) {
-	return MathJax(math).then(function(data) {
-		callback(data);
-	});
-}
